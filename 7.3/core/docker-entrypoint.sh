e1252b152b82acceb954192b6357a15d7f4f1bc8
TehesFR <ths.arborisoft@gmail.com>
2020-03-12 18:58:58 +0100

    upgrade php 7.3

--- 7.3/core/docker-entrypoint.sh
+++ 7.3/core/docker-entrypoint.sh
@@ -6,6 +6,33 @@ sed -i "s/MYSERVERNAME/$SERVERNAME/g" /etc/apache2/apache2.conf
 sed -i "s/MYSERVERALIAS/$SERVERALIAS/g" /etc/apache2/apache2.conf
 sed -i "s/MYDOCUMENTROOT/$DOCUMENTROOT/g" /etc/apache2/apache2.conf
 
+# Configure msmtp
+sed -i "s/MY_SMTP/$SMTP/g" /usr/local/etc/msmtprc
+sed -i "s/MY_EMAIL_DOMAIN/$EMAIL_DOMAIN/g" /usr/local/etc/msmtprc
+sed -i "s/MY_OUTGOING_ADDRESS/$OUTGOING_ADDRESS/g" /usr/local/etc/msmtprc
+
+# If docker secret apache2_configuration exists for custom/sensitive configuration, use the file instead
+if [ -f /run/secrets/apache2_configuration ]; then
+   echo "Using secret apache2_configuration"
+   rm /etc/apache2/apache2.conf
+   cp /run/secrets/apache2_configuration /etc/apache2/apache2.conf
+fi
+
+# If docker secret ssh_public_key exists copy it in /var/www/.ssh
+if [ -f /run/secrets/ssh_public_key ]; then
+   echo "Using secret ssh_public_key"
+   cp /run/secrets/ssh_public_key /var/www/.ssh/id_rsa.pub
+   chown -R web:www-data /var/www/.ssh
+fi
+
+# If docker secret ssh_private_key exists copy it in /var/www/.ssh
+if [ -f /run/secrets/ssh_private_key ]; then
+   echo "Using secret ssh_private_key"
+   cp /run/secrets/ssh_private_key /var/www/.ssh/id_rsa
+   chmod 600 /var/www/.ssh/id_rsa
+   chown -R web:www-data /var/www/.ssh
+fi
+
 # Set the apache user and group to match the host user.
 # This script will change the web UID/GID in the container from to 999 (default) to the UID/GID of the host user, if the current host user is not root.
 OWNER=$(stat -c '%u' /var/www/html)
@@ -15,8 +42,11 @@ USERNAME=web
 if [ "$OWNER" != "0" ]; then
   usermod -o -u $OWNER $USERNAME
   groupmod -o -g $GROUP www-data
-  chown -R --silent $USERNAME:www-data /var/www
+  #chown -R --silent $USERNAME:www-data /var/www
 fi
+
+# set msmtp permissions
+chmod 600 /usr/local/etc/msmtprc && chown web /usr/local/etc/msmtprc
 
 # Apache gets grumpy about PID files pre-existing
 rm -f /var/run/apache2/apache2.pid
